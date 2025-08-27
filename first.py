635942f7bb03b3f13b7f12a933167f9238068fe3
Dev <rishabsing2990@gmail.com>
2025-08-27 09:38:10 +0530

    third commit

--- first.py
+++ first.py
@@ -1,1 +1,16 @@
 print("hello")
+a=b=c=8
+print(id(a))
+print(id(b))
+print(id(c))
+print(a,b,c)
+ #unpack 
+f=[5,6,7]
+a,b,c=f
+print(a,b,c)
+#global variable
+def myfunc():
+    global x
+    x = "variable"
+myfunc()
+print (x)
